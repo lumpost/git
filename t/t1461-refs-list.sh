17bd4648c8d06428182ec537adf86f030f397585
Hopkins Zk <227235939+lumpost@users.noreply.github.com>
2025-11-28 12:14:00 +0100

    Delete t directory

--- t/t1461-refs-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-test_description='git refs list tests'
-
-. ./test-lib.sh
-
-git_for_each_ref='git refs list'
-. "$TEST_DIRECTORY"/for-each-ref-tests.sh
--- t/unit-tests/clar/test/selftest.h
+++
@@ -1,3 +0,0 @@
-#include "clar.h"
-
-extern const char *selftest_suite_directory;
